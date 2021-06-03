5439f21badc2568758665dcf04f9b69305b0605c
@dimas12triandi13 <dimas12triandi13@gmail.com>
2021-06-03 22:56:01 +0700

    Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,118 @@
-home
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>One-Banking</title>
+    <link rel="stylesheet" href="assets-homepage/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli">
+    <link rel="stylesheet" href="assets-homepage/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets-homepage/css/styles.min.css">
+</head>
+
+<body id="page-top">
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
+        <div class="container"><a class="navbar-brand text-dark js-scroll-trigger" href="#page-top">One-<strong>Banking</strong></a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler float-right" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-dark ml-auto">
+                    <li class="nav-item"><a class="nav-link text-dark js-scroll-trigger" href="#download">home</a></li>
+                    <li class="nav-item"><a class="nav-link text-dark js-scroll-trigger" href="#features">About Us<br></a></li>
+                    <li class="nav-item"><a class="nav-link text-dark js-scroll-trigger" href="#contact">Contact</a></li>
+                </ul><button class="btn btn-aktif" type="button">Log in</button>
+            </div>
+        </div>
+    </nav>
+    <header id="page-top" class="mast">
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-lg-7 my-auto">
+                    <div class="mx-auto header-content">
+                        <h1 class="mb-5, font-weight-bold"><br>Easy Transact With One Click<br></h1>
+                        <h3><br>transactions faster in order to shorten the time, no need to bother coming to the bank with just one click to transact<br><br></h3><a class="btn btn-outline-warning btn-xl js-scroll-trigger btn-aktif" role="button" href="#download">Get Started<br></a>
+                    </div>
+                </div>
+                <div class="col-lg-5 my-auto"><img class="img-size" src="assets-homepage/img/transfer.svg"></div>
+            </div>
+        </div>
+    </header>
+    <section id="download" class="download bg-custom">
+        <div class="container align-content-sm-center">
+            <h3 class="display-4 text-center text-light font-weight-bold"><br>Our Company<br></h3>
+            <h5 class="text-center text-light"><br>Our app is available on any mobile device! Download now to get started!<br><br></h5>
+        </div>
+    </section>
+    <section id="features" class="features">
+        <div class="container">
+            <div class="text-center section-heading">
+                <h2>Our Core Value<br><br></h2>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col col-lg-4">
+                    <div class="card card-custom shadow-lg text-center">
+                        <div class="card-body">
+                            <h4 class="card-title text-bold"><br>One Click Transaction<br><br></h4>
+                            <p class="card-text"><br>With just one click your transaction is complete<br><br></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-lg-4">
+                    <div class="card card-custom shadow-lg text-center">
+                        <div class="card-body">
+                            <h4 class="card-title text-bold"><br>Financial Advice<br><br></h4>
+                            <p class="card-text"><br>Become your trusted financial advisor<br><br></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-lg-4">
+                    <div class="card card-custom shadow-lg text-center">
+                        <div class="card-body">
+                            <h4 class="card-title text-bold"><br>Transparency<br><br></h4>
+                            <p class="card-text"><br>You can know all your transactions<br><br></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="contact" class="contact bg-color-custom">
+        <div class="container">
+            <h1><span><br>Dream Until You Financial Dreams Come True<br><br></span></h1>
+            <ul class="list-inline list-social">
+                <li class="list-inline-item social-twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li class="list-inline-item social-facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li class="list-inline-item social-google-plus"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+            </ul>
+        </div>
+    </section>
+    <footer class="bg-color-purple">
+        <div class="container">
+            <h5></h5>
+            <h3 class="text-left">One-<strong>Banking</strong></h3>
+            <p class="text-left">Your money solution<br></p>
+            <h3 class="text-right" style="margin-top: -60px">Contact Us</h3>
+            <ul class="list-inline text-right list-social font-weight-bolder">
+                <li class="list-inline-item social-twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li class="list-inline-item social-facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li class="list-inline-item social-google-plus"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+            </ul>
+            <p>© One-<strong>Banking</strong> 2021. All Rights Reserved.</p>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Privacy</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">FAQ</a></li>
+            </ul>
+        </div>
+    </footer>
+    <script src="assets-homepage/js/jquery.min.js"></script>
+    <script src="assets-homepage/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets-homepage/js/script.min.js"></script>
+</body>
+
+</html>
+</body>
+</html>
