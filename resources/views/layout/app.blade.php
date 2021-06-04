2c721f2af45edf5551704f27b6fc5094e7fbdd37
carissafarry <cazzahra31@gmail.com>
2021-06-04 19:30:03 +0700

    ubah struktur home page

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -18,13 +18,28 @@
     
     @yield('content')
     
+    
+    <footer class="bg-color-purple">
+        <div class="container">
+            <h5></h5>
+            <h3 class="text-left">One-<strong>Banking</strong></h3>
+            <p class="text-left">Your money solution<br></p>
+            <h3 class="text-right" style="margin-top: -60px">Contact Us</h3>
+            <ul class="list-inline text-right list-social font-weight-bolder">
+                <li class="list-inline-item social-twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li class="list-inline-item social-facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li class="list-inline-item social-google-plus"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+            </ul>
+            <p>© One-<strong>Banking</strong> 2021. All Rights Reserved.</p>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Privacy</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">FAQ</a></li>
+            </ul>
+        </div>
+    </footer>
+
     @include('layout.footer')
-    <script src="assets-homepage/js/jquery.min.js"></script>
-    <script src="assets-homepage/bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-    <script src="assets-homepage/js/script.min.js"></script>
 </body>
 
-</html>
-</body>
 </html>
