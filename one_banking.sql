13cc9953408da6a2702e414683f0a78a0bf50158
carissafarry <cazzahra31@gmail.com>
2021-06-09 12:21:20 +0700

    file sql

--- one_banking.sql
+++ one_banking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Bulan Mei 2021 pada 16.38
+-- Waktu pembuatan: 09 Jun 2021 pada 07.18
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.3.14
 
@@ -21,6 +21,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `one_banking`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `banks`
+--
+
+CREATE TABLE `banks` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name_bank` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `banks`
+--
+
+INSERT INTO `banks` (`id`, `name_bank`, `created_at`, `updated_at`) VALUES
+(1, 'BRI', NULL, NULL),
+(2, 'Mandiri', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -55,9 +76,11 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+(1, '2013_05_28_144749_create_banks_table', 1),
+(2, '2014_10_12_000000_create_users_table', 1),
+(3, '2014_10_12_100000_create_password_resets_table', 1),
+(4, '2019_08_19_000000_create_failed_jobs_table', 1),
+(5, '2021_05_28_144650_create_transactions_table', 1);
 
 -- --------------------------------------------------------
 
@@ -74,23 +97,72 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `sender` bigint(20) UNSIGNED DEFAULT NULL,
+  `receiver` bigint(20) UNSIGNED DEFAULT NULL,
+  `date` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` int(11) NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_receiver` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `receiver_bank_type` bigint(20) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `transactions`
+--
+
+INSERT INTO `transactions` (`id`, `sender`, `receiver`, `date`, `amount`, `description`, `email_receiver`, `receiver_bank_type`, `created_at`, `updated_at`) VALUES
+(1, NULL, NULL, '09-06-2021', 100000, 'Top Up', 'carissa@gmail.com', 1, '2021-06-08 21:17:38', '2021-06-08 21:17:38'),
+(2, NULL, NULL, '09-06-2021', 50000, 'Top Up', 'carissa@gmail.com', 1, '2021-06-08 21:37:03', '2021-06-08 21:37:03'),
+(3, NULL, NULL, '09-06-2021', 50000, 'Top Up', 'carissa@gmail.com', 1, '2021-06-08 21:39:10', '2021-06-08 21:39:10'),
+(4, NULL, NULL, '09-06-2021 04:44:26', 20000, 'Top Up', 'carissa@gmail.com', 1, '2021-06-08 21:44:26', '2021-06-08 21:44:26'),
+(5, NULL, NULL, '09-06-2021 04:51:59', 35000, 'Top Up', 'carissa@gmail.com', 1, '2021-06-08 21:51:59', '2021-06-08 21:51:59'),
+(6, NULL, NULL, '09-06-2021 04:54:34', 5000, 'Top Up', 'carissa@gmail.com', 1, '2021-06-08 21:54:34', '2021-06-08 21:54:34'),
+(7, NULL, NULL, '09-06-2021 04:54:41', 10000, 'Top Up', 'carissa@gmail.com', 1, '2021-06-08 21:54:41', '2021-06-08 21:54:41'),
+(8, NULL, NULL, '09-06-2021 04:58:45', 10000, 'Top Up', 'carissa@gmail.com', 1, '2021-06-08 21:58:45', '2021-06-08 21:58:45');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `account_number` int(11) DEFAULT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `balance` int(11) NOT NULL DEFAULT 0,
+  `bank_type` bigint(20) UNSIGNED DEFAULT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `account_number`, `name`, `email`, `email_verified_at`, `password`, `balance`, `bank_type`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'Carissa', 'carissa@gmail.com', NULL, '$2y$10$b1n4ZuM7aLANX0F6zLGzy.5U8HMo1vB8IQKXOmF/DIsawhnN5NOzy', 275000, NULL, NULL, '2021-06-08 21:17:08', '2021-06-08 21:58:45');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `banks`
+--
+ALTER TABLE `banks`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `failed_jobs`
@@ -112,15 +184,31 @@ ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
+-- Indeks untuk tabel `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `transactions_sender_foreign` (`sender`),
+  ADD KEY `transactions_receiver_foreign` (`receiver`),
+  ADD KEY `transactions_receiver_bank_type_foreign` (`receiver_bank_type`);
+
+--
 -- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD KEY `users_bank_type_foreign` (`bank_type`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `banks`
+--
+ALTER TABLE `banks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
@@ -132,13 +220,37 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_receiver_bank_type_foreign` FOREIGN KEY (`receiver_bank_type`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `transactions_receiver_foreign` FOREIGN KEY (`receiver`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `transactions_sender_foreign` FOREIGN KEY (`sender`) REFERENCES `users` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_bank_type_foreign` FOREIGN KEY (`bank_type`) REFERENCES `banks` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
