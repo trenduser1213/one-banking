24e2d392bf5905a872f01e5962db1f7c19a4c07a
carissafarry <cazzahra31@gmail.com>
2021-06-15 19:21:12 +0700

    menyerah

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\TransferController;
 use App\Http\Controllers\TopUpController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
